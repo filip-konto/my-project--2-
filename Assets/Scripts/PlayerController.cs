0f0ebff28520bf738cffe10b2d3322f40b0779a2
filip-konto <ilovelife87@gmail.com>
2026-03-31 19:43:06 +0200

    Added debugging raycast and added raycast

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,8 @@ public class PlayerController : MonoBehaviour
     Vector3 velocity;
     CharacterController characterController;
     // Start is called before the first frame update
+    public Transform groundCheck;
+    public LayerMask groundMask;
     void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -26,5 +29,31 @@ public class PlayerController : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         characterController.Move(move * speed * Time.deltaTime);
+
+        RaycastHit hit;
+        Debug.DrawRay(
+        groundCheck.position,
+        transform.TransformDirection(Vector3.down) * 0.4f,
+        Color.red
+        );
+        if (Physics.Raycast(groundCheck.position, transform.TransformDirection(Vector3.down), out hit, 0.4f, groundMask))
+        {
+            string terrainType;
+            terrainType = hit.collider.gameObject.tag;
+            
+
+            switch (terrainType)
+            {
+                default:
+                    speed = 12;
+                    break;
+                case "Low":
+                    speed = 3;
+                    break;
+                case "High":
+                    speed = 20;
+                    break;
+            }
+        }
     }
 }
